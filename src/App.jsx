a6fc5e36b7322331b778e879026e5bd59816c87f
Matthew Molli <mattjmolli@gmail.com>
2024-01-19 17:29:01 -0500

    Initial App set up with landing page

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Trivia Time</h1>
+      <p>Need a break? Click below for a quick round of trivia.</p>
+      <div className="card">
+        <button>
+          Start Round
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
