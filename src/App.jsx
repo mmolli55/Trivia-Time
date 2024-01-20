7ef596110dde150597f349ab5cbd4b9e6f13c3c4
Matthew Molli <mattjmolli@gmail.com>
2024-01-20 17:29:03 -0500

    added API call
    making request to open Trivia Data base and initializing state based on response

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import StartScreen from './components/StartScreen.jsx'
 
 function App() {
   const [showStartPage, setShowStartPage] = useState(true)
+  const [triviaData, setTriviaData] = useState([])
+  
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
+      .then(res => res.json())
+      .then(data => setTriviaData(data.results))
+  }, [])
 
   function startNewGame() {
-    setShowStartPage(false)
+    console.log(triviaData)
+    //setShowStartPage(false)
   }
+  
   return (
     <>
       {showStartPage && <StartScreen startNewGame={startNewGame}/>}
