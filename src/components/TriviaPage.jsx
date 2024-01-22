507399af02aa48c2e3a9a5502c8a427d2dcef18e
Matthew Molli <mattjmolli@gmail.com>
2024-01-22 03:58:40 -0500

    added shuffle function to randomize possible answer order

--- src/components/TriviaPage.jsx
+++ src/components/TriviaPage.jsx
@@ -25,8 +25,29 @@ function TriviaPage(props) {
         const incorrectAnswer = item.incorrect_answers[i]
         possibleAnswers.push({value: incorrectAnswer, isCorrect: false})
       }
+      
+      /* Randomize array in-place using Fisher-Yates (aka Knuth) Shuffle */
+      function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex > 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
 
-      return possibleAnswers
+      const shuffledAnswers = shuffle(possibleAnswers)
+
+      return shuffledAnswers
     })
 
     return (
