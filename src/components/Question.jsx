55a0d933316a1d0b53d05e62d1799cb4d52ddf35
Matthew Molli <mattjmolli@gmail.com>
2024-01-22 03:47:02 -0500

    added display of all possible answers for each question

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,9 +1,20 @@
 function Question(props) {
+
+    let answerOptionsElements = []
+    for(let i = 0; i < props.answerOptions.length; i++) {
+        let answerOption = props.answerOptions[i].value
+        answerOptionsElements.push(
+          <button className="answer-option" key={i}>{answerOption}</button>
+        )
+    }
+
     return (
         <>
-          <p>{props.question}</p>
+          <p className="question-text">{props.question}</p>
           <div className="answer-container">
+            {answerOptionsElements}
           </div>
+          <hr />
         </>
     )
 }
--- src/components/TriviaPage.jsx
+++ src/components/TriviaPage.jsx
@@ -2,54 +2,54 @@ import Question from "./Question.jsx"
 
 function TriviaPage(props) {
     function test() {
-      console.log(questions)
+      console.log(answerOptions)
     }
 
     const triviaObject = props.triviaData
-
+    
+    /* Generates array of all quiz questions */
     const questions = triviaObject.map(item => item.question)
+    
+    /* Generates array of arrays, each nested array contains all possible answers 
+    for an individual question */
+    const answerOptions = triviaObject.map(item => {
+      let possibleAnswers = []
 
-
-
-
-
-    /*let answerOptions = []
-
-    const correctAnswer = {
-        value: `${triviaObject[0].correct_answer}`,
+      const correctAnswer = {
+        value: item.correct_answer,
         isCorrect: true
-    }
-    answerOptions.push(correctAnswer)
+      }
+      possibleAnswers.push(correctAnswer)
 
-    for(let i = 0; i < 3; i++) {
-      const incorrectAnswer = triviaObject[0].incorrect_answers[i]
-      answerOptions.push({value: incorrectAnswer, isCorrect: false})
-    }
+      for(let i = 0; i < item.incorrect_answers.length; i++) {
+        const incorrectAnswer = item.incorrect_answers[i]
+        possibleAnswers.push({value: incorrectAnswer, isCorrect: false})
+      }
 
-    let answerOptionsElements = []
-    for(let i = 0; i < answerOptions.length; i++) {
-        let answerOption = answerOptions[i].value
-        answerOptionsElements.push(
-          <button className="answer-option" key={i}>{answerOption}</button>
-        )
-    }*/
+      return possibleAnswers
+    })
 
     return (
         <>
           <Question 
             question={questions[0]}
+            answerOptions={answerOptions[0]}
           />
           <Question 
             question={questions[1]}
+            answerOptions={answerOptions[1]}
           />
           <Question 
             question={questions[2]}
+            answerOptions={answerOptions[2]}
           />
           <Question 
             question={questions[3]}
+            answerOptions={answerOptions[3]}
           />
           <Question 
             question={questions[4]}
+            answerOptions={answerOptions[4]}
           />
           <button className="check-answers" onClick={test}>
                 Check Answers
